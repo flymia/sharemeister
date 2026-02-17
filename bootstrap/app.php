4a334a604387a0c530a58c4d2fb98dade8a6ab13
Marc Sauer <info@marcsauer.net>
2026-02-17 21:25:17 +0100

    Fixed a TLS error message when deploying behind reverse proxy

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->trustProxies(at: env('TRUSTED_PROXIES', '*'));
     })
     ->withMiddleware(function (Middleware $middleware) {
     $middleware->append(\App\Http\Middleware\CheckInstallation::class);
--- config/version.php
+++ config/version.php
@@ -2,6 +2,6 @@
 // config/version.php
 
 return [
-    'tag' => '1.0.1',
-    'build' => '20260213',
+    'tag' => '1.0.2',
+    'build' => '20260217',
 ];
