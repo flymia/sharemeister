49b805581dc5a29fb61441cf0795fdf51c0bf71c
Marc Sauer <info@marcsauer.net>
2026-02-22 11:08:39 +0100

    Refactored API route, cleaned up SXCU generation

--- routes/api.php
+++ routes/api.php
@@ -2,16 +2,19 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
 use App\Http\Controllers\ScreenshotController;
 use App\Http\Controllers\Api\SystemMetricsController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
-Route::middleware('auth:sanctum')->post('/upload', [ScreenshotController::class, 'apiUpload'])->name('api.screenshot.upload');
-Route::middleware('auth:sanctum')->post('/upload/raw', [ScreenshotController::class, 'apiUploadRaw'])->name('api.screenshot.upload.raw');
-
-
 Route::get('/health', [SystemMetricsController::class, 'index']);
+
+
+Route::middleware('auth:sanctum')->group(function () {
+    
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    Route::post('/upload', [ScreenshotController::class, 'apiUpload'])->name('api.screenshot.upload');
+    Route::post('/upload/raw', [ScreenshotController::class, 'apiUploadRaw'])->name('api.screenshot.upload.raw');
+    
+});
